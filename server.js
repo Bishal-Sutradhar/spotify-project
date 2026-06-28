05c6cfb4c93e235585cfb9baa148d8df7024a036
Bishal Sutradhar <bishalsutradhar2006@gmail.com>
2026-06-28 20:13:47 +0530

    Initial commit-basic structure

---
+++ server.js
@@ -0,0 +1,10 @@
+require("dotenv").config({quiet: true})
+
+const app = require('./src/app')
+const connectDB = require("./src/db/db")
+
+connectDB()
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server started at port ${process.env.PORT}`)
+})
