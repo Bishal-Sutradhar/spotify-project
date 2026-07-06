131e5169138ea2047cc69c5c6f86c458484579a3
Bishal Sutradhar <bishalsutradhar2006@gmail.com>
2026-07-07 02:51:27 +0530

    Add create album feature

--- src/controller/music.controller.js
+++ src/controller/music.controller.js
@@ -1,4 +1,5 @@
 const musicModel = require("../models/music.model")
+const albumModel = require("../models/album.model")
 const jwt = require("jsonwebtoken")
 const uploadFile = require("../services/storage.service")
 
@@ -40,4 +41,38 @@ const createMusic = async (req, res) => {
 
 }
 
-module.exports = { createMusic }
+const createAlbum = async (req, res) => {
+
+    if(req.user.role !== "artist") {
+        return res.status(403).json({
+            message: "You do not have access to create an album"
+        })
+    }
+
+    const { title, musics } = req.body
+
+    try {
+        const album = await albumModel.create({
+            title,
+            musics,
+            artist: req.user.id
+        })
+
+        res.status(201).json({
+            message: "Album created successfully",
+            album: {
+                title,
+                musics,
+                artist: req.user.id
+            }
+        })
+    } catch(err) {
+        console.error(`Error while creating album: ${err.message}`)
+
+        res.status(500).json({
+            message: "Internal server error"
+        })
+    }
+}
+
+module.exports = { createMusic, createAlbum }
