29ce81b4cbca12768d54483107ecbd3f8efdeb80
Bishal Sutradhar <bishalsutradhar2006@gmail.com>
2026-07-08 22:10:38 +0530

    Remove redundant ImageKit configuration properties

--- src/services/storage.service.js
+++ src/services/storage.service.js
@@ -1,9 +1,7 @@
 const ImageKit = require('@imagekit/nodejs')
 
 const client = new ImageKit({
-  privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
-  publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
-  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY
 })
 
 
